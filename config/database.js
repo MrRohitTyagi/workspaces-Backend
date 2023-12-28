15f9eaabdcb8c4d29b6cd79823a3618cea147cf3
Rohit <rohit.tyagi@thoughts2binary.com>
2023-12-28 10:01:27 +0530

    11

--- config/database.js
+++ config/database.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
-  let dataa;
   await mongoose
     .connect(process.env.MONGO_STRING)
     .then((data) => {
       console.log(`MondoDB connected with server ${data.connection.host}`);
-      dataa = data.connection.host;
     })
     .catch((err) => {
       console.log(err);
     });
-  return dataa;
 };
 
 module.exports = connectDatabase;
--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
-let temp;
 //connect database
-let data = connectDatabase();
-data.then((res) => {
-  temp = res;
-});
-
-app.get("/", (req, res) => {
-  res.send({ success: true, msg: "base route", temp });
-});
+connectDatabase();
 
 app.listen(4000, () => {
   console.log(`server is running at  http://localhost:${4000}`);
