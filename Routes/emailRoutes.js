322f86fc2e01650bc273bde6caf28ec3992ca74e
Rohit <rohit.tyagi@thoughts2binary.com>
2023-12-29 21:39:18 +0530

    13

--- Routes/emailRoutes.js
+++ Routes/emailRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { createEmail, deleteEmail } = require("../controllers/emailController");
+const {
+  createEmail,
+  deleteEmail,
+  deleteEmailSent,
+  updateEmail,
+} = require("../controllers/emailController");
 const router = express.Router();
 
 router.route("/create").post(createEmail);
 router.route("/delete/:id/:email").delete(deleteEmail);
+router.route("/delete-sent/:id").delete(deleteEmailSent);
+router.route("/update-email").put(updateEmail);
 
 module.exports = router;
--- controllers/emailController.js
+++ controllers/emailController.js
@@ -23,12 +23,59 @@ exports.deleteEmail = async (req, res) => {
   try {
     const newEmail = await EMAIL.findByIdAndUpdate(
       id,
-      { $pull: { recipients: emailToRemove } },
+      {
+        $pull: { recipients: emailToRemove },
+        $pull: { starredBy: emailToRemove },
+        $pull: { archivedBy: emailToRemove },
+      },
       { new: true }
     );
+
+    res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+exports.updateEmail = async (req, res) => {
+  console.log("req.body", req.body);
+  const { updateingKey, _id, ...rest } = req.body;
+  try {
+    const newEmail = await EMAIL.findByIdAndUpdate(
+      _id,
+      { [updateingKey]: rest[updateingKey] },
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      response: newEmail,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+exports.deleteEmailSent = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const newEmail = await EMAIL.findByIdAndUpdate(
+      id,
+      { sender: "EMPTY" },
+      { new: true }
+    );
+
     if (newEmail.recipients.length === 0) {
       await EMAIL.findByIdAndDelete(newEmail._id);
     }
+
     res.status(200).json({
       success: true,
     });
